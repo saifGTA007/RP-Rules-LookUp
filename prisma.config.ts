a3f36792b4d8de45db09ba55268b85019afe154e
saif-spec <saifgtaiv@gmail.com>
2026-01-31 18:43:04 +0100

    update

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from '@prisma/config';
 export default defineConfig({
   datasource: {
     // Check all possible variable names
-    url: process.env.PRISMA_DATABASE_URL || process.env.POSTGRES_PRISMA_URL || process.env.DATABASE_URL,
+    url: process.env.POSTGRES_URL,
   },
 });
